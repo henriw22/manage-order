e2cc319f4c85312143ee72674c6befdc01190c08
Henri William <83308210+henriw22@users.noreply.github.com>
2022-09-13 22:03:19 -0700

    installed mui

--- react-front-end/src/pages/HomePage.jsx
+++ react-front-end/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { Stack, Button } from '@mui/material';
 
 const HomePage = () => {
   return (
-  <div>
+  <Stack width="50%" margin="auto">
     <h1>This is the Home Page</h1>
     <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-  </div>
+    <Button sx={{backgroundColor: "red", width: "250px"}}>Test MUI</Button>
+  </Stack>
   );
 };
 export default HomePage;
