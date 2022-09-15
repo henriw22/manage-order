bd27925345bf1507cbf25e2e4d1946295faa3c89
Henri William <83308210+henriw22@users.noreply.github.com>
2022-09-14 22:13:30 -0700

    basic form

--- react-front-end/src/components/OpenOrder/OrderForm.jsx
+++ react-front-end/src/components/OpenOrder/OrderForm.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Stack } from '@mui/material';
+import { Stack, TextField } from '@mui/material';
 
 const OrderForm = () => {
   return (
-    <Stack width="100%" sx={{ margin: "1em" }}>
+    <Stack width="100%" margin="1em">
       <h1>Mie Ayam</h1>
+      <Stack marginTop="1em">
+        <span>How many would you like to order?</span>
+        <TextField
+          id="standard-number"
+          label="Quantity"
+          type="number"
+          sx={{ marginTop: '1em' }}
+        />
+      </Stack>
+      <Stack marginTop="1em">
+        <span>Pick Up Location</span>
+        <TextField placeholder="Outlined" />
+      </Stack>
+      <Stack marginTop="1em">
+        <span>Pick Up Time</span>
+        <TextField placeholder="Pick Up Time" />
+      </Stack>
     </Stack>
   );
 };
