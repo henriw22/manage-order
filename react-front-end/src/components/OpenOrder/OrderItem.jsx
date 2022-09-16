d1703f608265561ed7e322d60a6c6fa8b4e35cb5
Henri William <83308210+henriw22@users.noreply.github.com>
2022-09-15 22:15:17 -0700

    testing simple dialog modal

--- react-front-end/src/components/OpenOrder/OrderItem.jsx
+++ react-front-end/src/components/OpenOrder/OrderItem.jsx
@@ -1,7 +1,30 @@
 import React, { useState } from "react";
-import { Card, CardHeader, CardMedia, CardContent, CardActions, Button, Typography } from '@mui/material';
+import { 
+  Button, 
+  Card, 
+  CardHeader, 
+  CardMedia, 
+  CardContent, 
+  CardActions,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Typography 
+} from '@mui/material';
 
 const OrderItem = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Card sx={{ width: 240, height: 400, margin: 1 }}>
       <CardHeader
@@ -20,11 +43,33 @@ const OrderItem = () => {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">
+        <Button size="small" onClick={handleClickOpen}>
           <Typography variant="button">
             Order This
           </Typography>
         </Button>
+        <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous
+            location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={handleClose} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
       </CardActions>
     </Card>
   );
