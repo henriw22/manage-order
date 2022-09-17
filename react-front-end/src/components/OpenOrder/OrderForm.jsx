9badf2c2b046b6848a0536f39216f507908950da
Henri William <83308210+henriw22@users.noreply.github.com>
2022-09-16 20:53:27 -0700

    changed the form to a mui dialog

--- react-front-end/src/components/OpenOrder/OrderForm.jsx
+++ react-front-end/src/components/OpenOrder/OrderForm.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Stack,
   TextField,
   FormControl,
@@ -10,7 +16,8 @@ import {
   SelectChangeEvent
 } from '@mui/material';
 
-const OrderForm = () => {
+const OrderForm = (props) => {
+  const { open, handleClose } = props;
   const [location, setLocation] = useState('');
   const [time, setTime] = useState('');
 
@@ -23,62 +30,79 @@ const OrderForm = () => {
   };
 
   return (
-    <Stack width="100%" margin="1em">
-      <h1>Mie Ayam</h1>
-      <Stack marginTop="1em">
-        <span>How many would you like to order?</span>
-        <FormControl sx={{ m: 1, minWidth: 120, paddingTop: 1 }}>
-          <TextField
-            id="standard-number"
-            label="Quantity"
-            type="number"
-          />
-          <FormHelperText></FormHelperText>
-        </FormControl>
-      </Stack>
-      <Stack marginTop="1em">
-        <span>Pick Up Location</span>
-        <FormControl sx={{ m: 1, minWidth: 120, paddingTop: 1 }}>
-          <InputLabel id="demo-simple-select-helper-label">Location</InputLabel>
-          <Select
-            labelId="demo-simple-select-helper-label"
-            id="demo-simple-select-helper"
-            value={location}
-            label="Location"
-            onChange={handleLocationChange}
-          >
-            <MenuItem value="">
-              <em>Please choose a pick up location</em>
-            </MenuItem>
-            <MenuItem value="Lougheed Mall">Lougheed Mall</MenuItem>
-            <MenuItem value="Metrotown">Metrotown</MenuItem>
-            <MenuItem value="Home">Home</MenuItem>
-          </Select>
-          <FormHelperText></FormHelperText>
-        </FormControl>
-      </Stack>
-      <Stack marginTop="1em">
-        <span>Pick Up Time</span>
-        <FormControl sx={{ m: 1, minWidth: 120, paddingTop: 1 }}>
-          <InputLabel id="demo-simple-select-helper-label">Time</InputLabel>
-          <Select
-            labelId="demo-simple-select-helper-label"
-            id="demo-simple-select-helper"
-            value={time}
-            label="Time"
-            onChange={handleTimeChange}
-          >
-            <MenuItem value="">
-              <em>Please choose a pick up time</em>
-            </MenuItem>
-            <MenuItem value="10">10 AM</MenuItem>
-            <MenuItem value="12">12 PM</MenuItem>
-            <MenuItem value="14">2 PM</MenuItem>
-          </Select>
-          <FormHelperText></FormHelperText>
-        </FormControl>
-      </Stack>
-    </Stack>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">
+        Mie Ayam
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          <Stack marginTop="1em">
+            <span>How many would you like to order?</span>
+            <FormControl sx={{ m: 1, minWidth: 120, paddingTop: 1 }}>
+              <TextField
+                id="standard-number"
+                label="Quantity"
+                type="number"
+              />
+              <FormHelperText></FormHelperText>
+            </FormControl>
+          </Stack>
+          <Stack marginTop="1em">
+            <span>Pick Up Location</span>
+            <FormControl sx={{ m: 1, minWidth: 120, paddingTop: 1 }}>
+              <InputLabel id="demo-simple-select-helper-label">Location</InputLabel>
+              <Select
+                labelId="demo-simple-select-helper-label"
+                id="demo-simple-select-helper"
+                value={location}
+                label="Location"
+                onChange={handleLocationChange}
+              >
+                <MenuItem value="">
+                  <em>Please choose a pick up location</em>
+                </MenuItem>
+                <MenuItem value="Lougheed Mall">Lougheed Mall</MenuItem>
+                <MenuItem value="Metrotown">Metrotown</MenuItem>
+                <MenuItem value="Home">Home</MenuItem>
+              </Select>
+              <FormHelperText></FormHelperText>
+            </FormControl>
+          </Stack>
+          <Stack marginTop="1em">
+            <span>Pick Up Time</span>
+            <FormControl sx={{ m: 1, minWidth: 120, paddingTop: 1 }}>
+              <InputLabel id="demo-simple-select-helper-label">Time</InputLabel>
+              <Select
+                labelId="demo-simple-select-helper-label"
+                id="demo-simple-select-helper"
+                value={time}
+                label="Time"
+                onChange={handleTimeChange}
+              >
+                <MenuItem value="">
+                  <em>Please choose a pick up time</em>
+                </MenuItem>
+                <MenuItem value="10">10 AM</MenuItem>
+                <MenuItem value="12">12 PM</MenuItem>
+                <MenuItem value="14">2 PM</MenuItem>
+              </Select>
+              <FormHelperText></FormHelperText>
+            </FormControl>
+          </Stack>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Disagree</Button>
+        <Button onClick={handleClose} autoFocus>
+          Agree
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 export default OrderForm;
--- react-front-end/src/components/OpenOrder/OrderItem.jsx
+++ react-front-end/src/components/OpenOrder/OrderItem.jsx
@@ -13,9 +13,10 @@ import {
   DialogTitle,
   Typography 
 } from '@mui/material';
+import OrderForm from "./OrderForm";
 
 const OrderItem = () => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -48,28 +49,7 @@ const OrderItem = () => {
             Order This
           </Typography>
         </Button>
-        <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
-          <Button onClick={handleClose} autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
+        <OrderForm open={open} handleClose={handleClose} />
       </CardActions>
     </Card>
   );
