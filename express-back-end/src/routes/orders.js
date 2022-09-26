8c433c9e56ef315b77f1f7c6d18efcf6146bbdcb
Henri William <83308210+henriw22@users.noreply.github.com>
2022-09-26 11:02:59 -0700

    added a post action on orders api

--- express-back-end/src/routes/orders.js
+++ express-back-end/src/routes/orders.js
@@ -13,5 +13,17 @@ module.exports = db => {
     });
   });
 
+  router.post("/orders", (req, res) => {
+
+    db.query(`
+      INSERT INTO orders (user_id, item_id, created_at, quantity, location)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *;
+    `, [req.body.user_id, req.body.item_id, req.body.created_at, req.body.quantity, req.body.location])
+    .then((result) => {
+      res.status(201).json(result.rows[0]);
+    });
+  });
+
   return router;
 };
