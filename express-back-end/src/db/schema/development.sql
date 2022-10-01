4476a693421e4e1614baa96d6c0ab78a95ef3620
Henri William <83308210+henriw22@users.noreply.github.com>
2022-10-01 09:26:59 -0700

    added more mock item data

--- express-back-end/src/db/schema/development.sql
+++ express-back-end/src/db/schema/development.sql
@@ -7,7 +7,8 @@ VALUES
 INSERT INTO order_items (name, price, order_quantity, description, img_url, created_at, order_date)
 VALUES
 ('Mie Ayam', '15', 50, 'Mie Ayam is a common Indonesian dish of seasoned yellow wheat noodles topped with diced chicken meat', '/images/mie_ayam.jpeg', '2022-08-01 10:00:00', '2022-10-01 10:00:00'),
-('Satay', '18', 75, 'Satay is an Indonesian dish of seasoned, skewered and grilled meat, served with peanut sauce.', '/images/satay.jpeg', '2022-09-01 10:00:00', '2022-12-01 10:00:00');
+('Satay', '18', 75, 'Satay is an Indonesian dish of seasoned, skewered and grilled meat, served with peanut sauce.', '/images/satay.jpeg', '2022-09-01 10:00:00', '2022-12-01 10:00:00'),
+('Nasi Goreng', '12', 30, 'Nasi Goreng is an Indonesian fried rice dish, usually cooked with pieces of meat and vegetables.', '/images/nasi_goreng.jpeg', '2022-09-01 10:00:00', '2022-12-01 10:00:00');
 
 INSERT INTO orders (user_id, item_id, created_at, quantity, location)
 VALUES
@@ -21,4 +22,7 @@ VALUES
 (1, 'Pick Up at 4 PM'),
 (1, 'Pick Up at 3 PM'),
 (2, 'Metrotown at 12 PM'),
-(2, 'Surrey at 3 PM');
+(2, 'Surrey at 3 PM'),
+(3, 'Downtown at 5 PM'),
+(3, 'Burnaby at 3 PM'),
+(3, 'Chinatown at 4 PM');
